719e0c11eeff780f085083500cf9a7f561fbdced
CommanderTempest <krukovtempest@gmail.com>
2024-03-21 15:19:25 -0500

    Character Movement & Camera Work
    +Add a character model
    +Added character movement
    +Added a camera that can look around

---
+++ Player/player.gd
@@ -0,0 +1,62 @@
+extends CharacterBody3D
+
+@export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.0
+@export var max_hitpoints := 100
+@export var speed := 8.0
+
+@onready var camera_pivot = $CameraPivot
+@onready var smooth_camera = $CameraPivot/SmoothCamera
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_motion := Vector2.ZERO
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	handle_camera_location()
+	# Add the gravity.
+	if not is_on_floor():
+		# if jumping, apply gravity
+		if (velocity.y >= 0):
+			velocity.y -= gravity * delta
+		# when falling apply more gravity
+		else:
+			velocity.y -= gravity * delta * fall_multiplier
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = sqrt(jump_height * 2.0 * gravity)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED: 
+		mouse_motion = -event.relative * 0.001
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+func handle_camera_location() -> void:
+	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clampf(
+		camera_pivot.rotation_degrees.x,
+		-90.0,
+		90.0
+	)
+	
+	mouse_motion = Vector2.ZERO
