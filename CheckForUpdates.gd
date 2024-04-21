4f3813b214174c3b5f5684616fad71c763bce6d7
CommanderTempest <krukovtempest@gmail.com>
2024-04-21 18:26:43 -0500

    HTTPRequest Works
    +HTTPRequest is getting response back from website

--- CheckForUpdates.gd
+++ CheckForUpdates.gd
@@ -1,14 +1,19 @@
-extends Node
-
-@onready var http_request = $HTTPRequest
+extends HTTPRequest
 
 func _ready():
-	http_request.request_completed.connect(_on_request_completed)
+	self.set_use_threads(true)
 	
 	# change request to ping my website, and get the latest update, then run comparison with current
 	# version, May run a singleton for current version?
-	http_request.request("https://api.github.com/repos/godotengine/godot/releases/latest")
+	#self.request_completed.connect(_on_request_completed)
+	var headers: PackedStringArray = []
+	self.request("http://localhost:9000/getversion", headers, HTTPClient.METHOD_GET)
 
-func _on_request_completed(result, response_code, headers, body):
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json["name"])
+func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
+#	var data = body.get_string_from_utf8()
+#	print(data)
+	#var json = JSON.parse_string(body)
+	#print(body)
+	print(body.get_string_from_utf8())
+	print(JSON.parse_string(body.get_string_from_utf8()))
+	
