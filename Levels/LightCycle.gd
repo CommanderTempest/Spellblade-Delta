06e1083b8d016ddf33bc0a42a751417923e96265
Tempest <krukovtempest@gmail.com>
2024-05-07 11:02:29 -0500

    Smoother Day/Night Cycle
    +The sun doesn't move in increments of 1 degrees on it's x axis anymore, it tweens in increments of 1 degree now

--- Levels/LightCycle.gd
+++ Levels/LightCycle.gd
@@ -10,13 +10,26 @@ func _ready():
 	start_cycle()
 
 func start_cycle():
+	var solar_move_time := 1
+	var solar_movement := self.rotation_degrees.x
+	
 	while not stopped_cycle:
-		self.rotation_degrees.x -= 1
+		solar_movement = self.rotation_degrees.x - 1
+		var sun_tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
+		sun_tween.tween_property(self, "rotation_degrees:x", solar_movement, solar_move_time)
 		if self.rotation_degrees.x <= -190:
+			# night
 			self.light_energy = 0
-			self.rotation_degrees.x = 60
 			environment.environment.background_energy_multiplier = 0.4
-		elif self.rotation_degrees.x <= 0:
+			self.visible = false
+			solar_movement = 60
+			solar_move_time = 60
+			await sun_tween.tween_property(self, "rotation_degrees:x", solar_movement, solar_move_time).finished
+			
+			# it's day again
+			solar_move_time = 1
+			self.visible = true
 			self.light_energy = 2
 			environment.environment.background_energy_multiplier = 1
+			
 		await get_tree().create_timer(WAIT_TIMER).timeout
