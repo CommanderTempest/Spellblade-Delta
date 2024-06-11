0e47c6846a580b2e1213c28d8b5362212b4d5081
Tempest <krukovtempest@gmail.com>
2024-06-11 09:07:01 -0500

    Added a note for SWeapon

--- Assets/Weapons/SWeapon.cs
+++ Assets/Weapons/SWeapon.cs
@@ -6,4 +6,8 @@ public partial class SWeapon : Node3D
     [Export] public float Length {get; private set;} = 5.0f;  // from how far you can hit a target
     [Export] public float Speed {get; private set;} = 2.0f;   // how quickly you can swing the weapon
     [Export] public float Damage {get; private set;} = 5.0f;  // how much damage a single swing does
+
+    /*
+        May need separate inherited classes for wands/swords/ other types?
+    */
 }
